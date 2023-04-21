79b1820f57a90ebc46391c955eac17a6ae65087e
s-nice <214653771@qq.com>
2023-04-21 18:25:01 +0800

    -

--- src/components/article/article.component.ts
+++ src/components/article/article.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core'
 import { $t } from 'src/locale';
 
 @Component({
-  selector: 'app-no-data',
-  templateUrl: './no-data.component.html',
-  styleUrls: ['./no-data.component.scss']
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
 })
 export class NoDataComponent {
   $t = $t
