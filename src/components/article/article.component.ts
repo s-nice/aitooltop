cd1d3cc755c6f625387406d2352e793954d867f3
s-nice <214653771@qq.com>
2023-04-22 21:47:45 +0800

    -

--- src/components/article/article.component.ts
+++ src/components/article/article.component.ts
@@ -3,13 +3,15 @@
 import { Component, OnInit, Input } from '@angular/core'
 import { ActivatedRoute } from '@angular/router';
 import { ArticleService } from '../../services/article';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss']
 })
+
 export class ArticleComponent implements OnInit {
   @Input() a: string;
   article: any;
@@ -23,5 +25,9 @@ export class ArticleComponent implements OnInit {
   ngOnInit(): void {
     const id = +this.a;
     this.articleService.getArticle(id).subscribe(article => this.article = article);
+  }
+
+  getSafeHtml(content) {
+    return this.sanitizer.bypassSecurityTrustHtml(this.article.content);
   }
 }
